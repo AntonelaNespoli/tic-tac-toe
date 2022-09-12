3725c8913983d10a97843bf5d7066b307cdcd98f
AntoNespoli93 <antonela.nespoli@gmail.com>
2022-09-11 21:39:47 -0300

    build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "@fontsource/plus-jakarta-sans";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "@fontsource/plus-jakarta-sans";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
